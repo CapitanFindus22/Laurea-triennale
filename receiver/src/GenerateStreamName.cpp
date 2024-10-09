894796e845cd5bdff68498c2c60626a17e9cf5ea
CapitanFindus <106431916+CapitanFindus22@users.noreply.github.com>
2024-10-09 10:52:53 +0200

    Delete receiver directory

--- receiver/src/GenerateStreamName.cpp
+++
@@ -1,9 +0,0 @@
-#include "main.hpp"
-
-// Combine base and number to create a name
-// Example: STREAM_ + 5 -> STREAM_5
-std::string GenerateStreamName(std::string base, size_t number)
-{
-
-    return base + std::to_string(number);
-}
--- receiver/src/global.cpp
+++
@@ -1,4 +0,0 @@
-#include "main.hpp"
-
-std::atomic<bool> running{true};
-size_t windowLength = 4;
