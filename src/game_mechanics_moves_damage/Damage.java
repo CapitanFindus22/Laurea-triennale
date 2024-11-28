2f5a0e58e4f39eea7e9f2aec653ce92bda8561f6
CapitanFindus <106431916+CapitanFindus22@users.noreply.github.com>
2024-11-28 19:17:46 +0100

    Delete src directory

--- src/game_mechanics_moves_damage/Damage.java
+++
@@ -1,7 +0,0 @@
-package game_mechanics_moves_damage;
-
-import game_mechanics_moves.MoveFunction;
-
-public interface Damage extends MoveFunction{
-	
-}
--- src/game_mechanics_moves_stats/StatsChange.java
+++
@@ -1,9 +0,0 @@
-package game_mechanics_moves_stats;
-
-import game_mechanics_moves.MoveFunction;
-
-public interface StatsChange extends MoveFunction {
-
-	public static final int MAX_TIME = 6;
-	
-}
